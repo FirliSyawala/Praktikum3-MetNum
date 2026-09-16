ee7848c4622903f4ecb03367a73cadb7bf04dcc0
FirliSyawala <firlisyawalad@gmail.com>
2026-09-16 22:13:31 +0700

    Add files via upload

---
+++ anonymous_func.m
@@ -0,0 +1,22 @@
+% anonymous function
+
+% squared
+squared = @(x) x.^2
+squared(9)
+squared(2:6)
+
+% addition
+addition = @(x, y) x + y
+addition(5, 7)
+
+% subtraction
+substraction = @(x, y) x - y
+substraction(8, 10)
+
+% multiplication
+multiplication = @(x, y) x * y
+multiplication(4, 2)
+
+% division
+division = @(x, y) x / y
+division(2000, 250)
